25c6b83d24ce58ccfc3fb7c81099aa0c4618e1c3
Karthik Venkataramani <karthikv@jpl.nasa.gov>
2022-12-10 18:37:56 -0800

    initial commit

--- codes/day5.py
+++ codes/day5.py
@@ -1,14 +1,23 @@
-test = False
+from collections import deque
+
+test = True
 
 if test:
-    input_file = '../inputs/day1_test'
+    input_file = '../inputs/day5_test'
 else:
-    input_file = '../inputs/day1_input'
+    input_file = '../inputs/day5_input'
 
 with open(input_file, 'r') as f:
     lines = f.readlines()
 
-lines = [int(l.strip()) for l in lines]
+# identify line containing stack numbers
+for i, l in enumerate(lines):
+    if '[' not in l:
+        break
 
+n_stacks = max([int(x) for x in l.split()])
+idxs = [i for i, x in enumerate(l) if (x!= ' ') and (x!='\n')]
+print(idxs)
 # Part 1 
+
 # Part 2
